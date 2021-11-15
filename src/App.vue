86304084da53ca5df3af3ae8854ff68c2da82a80
Dan Smestad <hockeydan25@gmail.com>
2021-11-14 19:32:17 -0600

    would you rather vue component app, initial

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,48 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template> <!-- Vue template parent component, used to show elements we add inside and updates to the browser development server updates browser -->
+  <div id="app"> <!--had to add this it was not on my version + the div. Parent, child is  -->
+
+    <h1>Would you rather??</h1>
+    <!-- reference to h2 component from would-you-rather template. lower case tag names with dashes identifies its a vue component.  -->
+    <would-you-rather 
+    v-bind:question="wyrQuestion"
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-changed="answerChanged"></would-you-rather> 
+     <!--data from wouldyourather gets bond to app. Prop data in child=answer1, Parent data wyrAnswer1   -->
+    <!-- message to be dispalayed on page is the selection from the binding. -->
+    <p> {{ userSelectionMessage }}</p>
+
+  </div><!-- END app DIV.-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//start js script
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    WouldYouRather
+  },
+  data(){  // function.
+    return{
+      wyrQuestion: 'Would you rather never eat cheese again or never drink anything sweet again?',
+      wyrAnswer1: 'eat cheese',
+      wyrAnswer2: 'Drink anything Sweet',
+      userSelectionMessage: ''  //empty string, nothing selected. would you rather app will send data to parent from child.
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      this.userSelectionMessage = `Thanks! you chose ${choice}`
+    }
+  } 
 }
+//END js script
 </script>
 
 <style>
+/* Start css style this side is not limited globally? No scope assigned. */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +50,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: cornflowerblue;
 }
+
+body{
+  background-color: darkgrey;
+}
+/* ending css Style app.vue */
 </style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,54 @@
+<template> <!-- vue template-->
+  <div class="wyr"><!--vue div  class wyr -->
+    <!--h2 content-->
+    <h2>Please make your choice!</h2>
+
+    <h3>{{ question }}</h3> <!-- I noticed if I had a space between the { { tag is not coding correctly. questionis blue or white. -->
+    <!-- inline elements radio button and label.  -->
+    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade">
+    <label>{{ answer1 }}</label>
+    <!-- radio buttons need values v-binds connects value from our answer without us typing it in again, we can reuse it.  -->
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade">
+    <label>{{ answer2 }}</label>
+
+  </div> <!-- vue template-->
+</template> <!-- vue template-->
+
+<script> //start js script
+export default {
+  name: 'WouldYouRather', // name edited here . prop is used to send data from the parent
+  props: {
+    question: String,
+    answer1: String,
+    answer2: String,   
+  },
+  data() { //function, returns object
+    return{ 
+      choice: ''  // we want this to be unique if we have 
+    }
+  },
+  methods:{//method needs help to get a message to the parent. Use "$emit" a vue method event/message 
+    choiceMade() {
+      this.$emit('answer-changed', this.choice)//can use anyname you like , message sent to-parent. use dash in name to avoid language mixups.
+    }
+  }
+}
+//END javascript 
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+// <style scoped>
+/* starting css Style, scoped limits global useage. per above this component only */
+h3 {
+  margin: 40px 0 0;
+}
+
+.wyr{
+  border: 5px black dashed;
+  background-color: chartreuse;
+}
+
+
+/* END css style */
+</style>
